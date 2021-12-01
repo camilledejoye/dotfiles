c74b4c7d0babae0deed77e8dab30bee44f9b27a4
Camille Dejoye <camilledejoye@gmail.com>
2021-12-01 19:40:26 +0100

    nvim: update cmp config after changes

--- config/nvim/lua/cdejoye/config/cmp.lua
+++ config/nvim/lua/cdejoye/config/cmp.lua
@@ -1,88 +1,33 @@
 local cmp = require('cmp')
 
--- Name of the sources as keys and text to show in the completion menu as values
--- The order helps define the source priority, see `sorting.priority_weight` options
-local sources = {
-  { name = 'nvim_lsp', label = '[LSP]' },
-  { name = 'neorg', label = '[Neorg]' },
-  { name = 'path', label = '[Path]' },
-  -- luasnip = { name = 'luasnip', label = '[LuaSnip]' },
-  { name = 'ultisnips', label = '[UltiSnips]' },
-  { name = 'buffer', label = '[Buffer]', opts = { get_bufnrs = function()
-    return { vim.api.nvim_get_current_buf() }
-    -- local buffers = {}
-
-    -- for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    --   if vim.api.nvim_buf_is_loaded(buf) then
-    --     table.insert(buffers, buf)
-    --   end
-    -- end
-
-    -- return buffers
-
-    -- local bufs = {}
-
-    -- for _, win in ipairs(vim.api.nvim_list_wins()) do
-    --   bufs[vim.api.nvim_win_get_buf(win)] = true
-    -- end
-
-    -- return vim.tbl_keys(bufs)
-  end} },
-  { name = 'spell', label = '[Spell]' },
-  { name = 'emoji', label = '[Emoji]' },
-}
-
-local source_names = {}
-for _, source in ipairs(sources) do
-  source_names[source.name] = source.label
-end
-
 vim.o.completeopt = 'menu,menuone,noselect'
 
--- local luasnip = require('luasnip')
-
--- local function luasnip_jump_prev()
---   return function(fallback)
---     if not luasnip.jump(-1) then
---       fallback()
---     end
---   end
--- end
-
--- local function luasnip_jump_next()
---   return function(fallback)
---     if not luasnip.jump(1) then
---       fallback()
---     end
---   end
--- end
-
--- local function select_or_choose_or_jump_prev()
---   return function(fallback)
---     if not cmp.select_prev_item() then
---       if luasnip.choice_active() then
---         luasnip.change_choice(-1)
---       else
---         luasnip_jump_prev()(fallback)
---       end
---     end
---   end
--- end
-
--- local function select_or_choose_or_jump_next()
---   return function(fallback)
---     if not cmp.select_next_item() then
---       if luasnip.choice_active() then
---         luasnip.change_choice(1)
---       else
---         luasnip_jump_next()(fallback)
---       end
---     end
---   end
--- end
-
 cmp.setup {
-  sources = vim.tbl_values(sources),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+  }, {
+    { name = 'path' },
+    { name = 'buffer', option = {
+      get_bufnrs = function()
+        local bufs = {}
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          bufs[vim.api.nvim_win_get_buf(win)] = true
+        end
+        return vim.tbl_keys(bufs)
+      end
+    }, keyword_length = 5 },
+    { name = 'spell', keyword_length = 5 },
+    { name = 'emoji' },
+    { name = 'ultisnips' },
+    { name = 'neorg' },
+  }),
+
+  sorting = {
+    comparators = {
+      -- Should help prioritizing suggestions closer to the current position
+      function(...) return require('cmp_buffer'):compare_locality(...) end,
+    },
+  },
 
   snippet = {
     expand = function(args)
@@ -92,25 +37,20 @@ cmp.setup {
   },
 
   mapping = {
-    -- ['<C-p>'] = cmp.mapping(select_or_choose_or_jump_prev(), { 'i', 's' }),
-    -- ['<C-n>'] = cmp.mapping(select_or_choose_or_jump_next(), { 'i', 's' }),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm {
+    ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+    ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    -- ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = false,
-    },
-    -- ['<Tab>'] = function(fallback)
-    --   if not luasnip.expand() then
-    --     fallback()
-    --   end
-    -- end,
-    -- ['<C-k>'] = cmp.mapping(luasnip_jump_prev(), { 'i', 's' }),
-    -- ['<C-j>'] = cmp.mapping(luasnip_jump_next(), { 'i', 's' }),
+    }),
   },
 
   documentation = {
@@ -126,7 +66,16 @@ cmp.setup {
       vim_item.kind = item_kinds[kind_num]
 
       -- set a name for each source
-      vim_item.menu = source_names[entry.source.name]
+      -- vim_item.menu = source_names[entry.source.name]
+      vim_item.menu = ({
+        buffer = '[Buffer]',
+        nvim_lsp = '[LSP]',
+        luasnip = '[LuaSnip]',
+        ultisnips = '[UltiSnips]',
+        nvim_lua = '[Lua]',
+        latex_symbols = '[Latex]',
+        neorg = '[Neorg]',
+      })[entry.source.name]
 
       -- set the detail as menu
       if 'nvim_lsp' == entry.source.name then
@@ -139,3 +88,19 @@ cmp.setup {
     end,
   },
 }
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' },
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path', keyword_length = 5 }
+  }, {
+    { name = 'cmdline', keyword_length = 5 },
+  })
+})
--- config/nvim/lua/cdejoye/plugins.lua
+++ config/nvim/lua/cdejoye/plugins.lua
@@ -121,6 +121,7 @@ return require('packer').startup({ function(use)
   use { -- Completion manager
     'hrsh7th/nvim-cmp',
     requires = {
+      'hrsh7th/cmp-cmdline',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp',
