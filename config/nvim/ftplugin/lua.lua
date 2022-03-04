36d1baf8dd41dd972a18ae5f162135274bb682b8
Camille Dejoye <camilledejoye@gmail.com>
2022-03-04 10:23:27 +0100

    nvim(lua): update filetype configuration

--- config/nvim/ftplugin/lua.lua
+++ config/nvim/ftplugin/lua.lua
@@ -1,5 +1,11 @@
 local bo = vim.bo
+local map = require('cdejoye.utils').map
 
 bo.tabstop = 2
 bo.softtabstop = bo.tabstop
 bo.shiftwidth = bo.tabstop
+bo.comments = ':---,:--'
+
+map('<Leader>tf', '<Plug>PlenaryTestFile>', 'n', { noremap = false })
+map('<Leader>tl', '<Plug>PlenaryTestFile>', 'n', { noremap = false })
+map('<Leader>tn', '<Plug>PlenaryTestFile>', 'n', { noremap = false })
