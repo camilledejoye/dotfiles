b142d181d3b50c8ae328bb0753c5b091e6c4f233
Camille Dejoye <camilledejoye@gmail.com>
2022-03-13 11:34:59 +0100

    nvim(lua): add mappings to run tests

--- config/nvim/ftplugin/lua.lua
+++ config/nvim/ftplugin/lua.lua
@@ -1,11 +1,29 @@
 local bo = vim.bo
-local map = require('cdejoye.utils').map
+local map = require("cdejoye.utils").map
 
 bo.tabstop = 2
 bo.softtabstop = bo.tabstop
 bo.shiftwidth = bo.tabstop
 bo.comments = ':---,:--'
 
-map('<Leader>tf', '<Plug>PlenaryTestFile>', 'n', { noremap = false })
-map('<Leader>tl', '<Plug>PlenaryTestFile>', 'n', { noremap = false })
-map('<Leader>tn', '<Plug>PlenaryTestFile>', 'n', { noremap = false })
+vim.cmd([[
+function! CdejoyePlenaryTestHarnessFile(testfile)
+  let l:opts = {}
+  let l:minimal_init = findfile('minimal_init.vim', '**')
+
+  if empty(l:minimal_init)
+    let l:opts['minimum'] = v:true
+  else
+    let l:opts['minimal_init'] = l:minimal_init
+  endif
+
+  call luaeval("require('plenary.test_harness').test_directory(_A[1], _A[2])", [a:testfile, l:opts])
+endfunction
+]])
+
+local test_current_file = [[
+<cmd>call CdejoyePlenaryTestHarnessFile(expand('%:p'))<CR>
+]]
+map("<Leader>tf", test_current_file, "n", { noremap = false })
+map("<Leader>tl", test_current_file, "n", { noremap = false })
+map("<Leader>tn", test_current_file, "n", { noremap = false })
