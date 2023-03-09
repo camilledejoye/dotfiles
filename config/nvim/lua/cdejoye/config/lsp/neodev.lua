572b65ce592d7829828b0cc2bed24c7e149456ec
Camille Dejoye <camilledejoye@gmail.com>
2023-03-09 12:43:33 +0100

    nvim(neodev): fix config and enable for custom directory
    The override allows me to enable the plugin when working on my forks and
    own plugins.

--- config/nvim/lua/cdejoye/config/lsp/neodev.lua
+++ config/nvim/lua/cdejoye/config/lsp/neodev.lua
@@ -4,9 +4,17 @@ function M.setup()
   if pcall(require, 'neodev') then
     -- Must be setup before lspconfig
     require('neodev').setup({
-      library = {
-        plugins = { 'neotest', 'nvim-dap-ui', 'nvim-lspconfig' },
-      },
+      -- Using a list like that was suggested somewhere, I can't recall where
+      -- But it ended up putting `lua.` in front of every `require` I made which was failing when running nvim
+      -- while necessary during dev to get completion so I don't do it again dude ;)
+      -- library = {
+      --   plugins = { 'neotest', 'nvim-dap-ui', 'nvim-lspconfig', 'plenary.nvim' },
+      -- },
+      override = function(root_dir, library) -- To have it enabled when working on plugins
+        if root_dir:match('^'..vim.fs.normalize('~/work/vim/plugins')) then
+          library.plugins = true
+        end
+      end,
     })
   end
 end
