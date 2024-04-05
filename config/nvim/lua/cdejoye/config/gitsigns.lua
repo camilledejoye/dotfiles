356334f4e5da9934a303cfa4dfb38894e75a2778
Camille Dejoye <camilledejoye@gmail.com>
2024-04-05 17:43:51 +0200

    nvim(gitsign): update configuration

--- config/nvim/lua/cdejoye/config/gitsigns.lua
+++ config/nvim/lua/cdejoye/config/gitsigns.lua
@@ -4,28 +4,46 @@ require('gitsigns').setup {
     change = { text = '~' },
   },
 
-  keymaps = {
-    -- Default keymap options
-    noremap = true,
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+    local bmap = function (mode, lsh, rsh, opts)
+      require('cdejoye.utils').bmap(bufnr, lsh, rsh, mode, opts)
+    end
 
-    ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
-    ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
+    -- Navigation
+    bmap('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
 
-    ['n <Leader>sh'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ['v <Leader>sh'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    -- ['n <Leader>uh'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ['n <Leader>uh'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ['v <Leader>uh'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    -- ['n <Leader>Rh'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-    ['n <Leader>ph'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    -- ['n <Leader>bh'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
-    ['n <Leader>Sh'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
-    -- ['n <Leader>Uh'] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
+    bmap('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
 
-    -- Text objects
-    ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-    ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
-  },
+    -- Actions
+    bmap('n', '<leader>sh', gs.stage_hunk)
+    bmap('v', '<leader>sh', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    bmap('n', '<leader>uh', gs.reset_hunk)
+    bmap('v', '<leader>uh', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    bmap('n', '<leader>Rh', gs.reset_buffer)
+    bmap('n', '<leader>ph', gs.preview_hunk)
+    bmap('n', '<leader>bh', function() gs.blame_line{full=true} end)
+    bmap('n', '<leader>Sh', gs.stage_buffer)
+    bmap('n', '<leader>Uh', gs.reset_buffer_index)
+
+    bmap('n', '<leader>hu', gs.undo_stage_hunk)
+    bmap('n', '<leader>Uh', gs.reset_buffer_index)
+    bmap('n', '<leader>tb', gs.toggle_current_line_blame)
+    bmap('n', '<leader>hd', gs.diffthis)
+    bmap('n', '<leader>hD', function() gs.diffthis('~') end)
+    bmap('n', '<leader>td', gs.toggle_deleted)
+
+    -- Text object
+    bmap('ox', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end,
 
   watch_gitdir = {
     interval = 250,
