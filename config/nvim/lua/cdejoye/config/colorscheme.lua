8040e3793b7af4649b3d410a882516e329485621
Camille Dejoye <camilledejoye@gmail.com>
2025-07-20 21:02:51 +0200

    nvim(colorscheme): group customization of base16 in one place
    I had some customization split around.
    I think I wanted to keep them close to the config of the associated
    plugins.
    
    Now, I think it doesn't matter. I can manipulate the highlight groups
    even without the plugin being there.
    So I think it's more link to the base16 themes than the plugins.
    
    I also realized not all customization would work when changing
    colorscheme during a session.
    They now all uses the `Colorscheme` event.

--- config/nvim/lua/cdejoye/config/colorscheme.lua
+++ config/nvim/lua/cdejoye/config/colorscheme.lua
@@ -1,35 +1,3 @@
--- Tweaks the colors to my taste with a base16 theme
-local function customize_theme()
-  local colors = require('base16-colorscheme').colors
-  local cursorline = colors.base01
-  local visual = colors.base02
-  local comment = colors.base03
-  local blue = colors.base0D
-
-  local hi = require('cdejoye.utils').hi
-
-  hi('FloatBorder', { guifg = blue })
-
-  hi('EndOfBuffer', { guifg = 'bg' }) -- Hide the ~ in the number column
-  hi('VertSplit', { guifg = visual }) -- Color of the border between vertival splits
-
-  hi('LineNr', { guifg = comment }) -- Color of the line numbers
-  hi('CursorLineNr', { guifg = blue, guibg = 'bg' }) -- Color of the current line number
-
-  hi('Pmenu', { guifg = comment, guibg = cursorline, gui = 'italic' }) -- Completion menu
-  hi('PmenuSel', { guifg = 'none', guibg = 'bg', gui = 'bold' }) -- Selected item
-  hi('PmenuSbar', 'Pmenu') -- Completion menu scrollbar
-  hi('PmenuThumb', { guibg = visual }) -- Completion menu scrollbar's button
-  -- Custom ones to use for floating window showing documentation during completion
-  hi('PmenuBorder', 'PmenuInvisibleBorder')
-  -- Hide the borders by using the same fg and bg but keep the padding having by using borders
-  hi('PmenuInvisibleBorder', { guifg = cursorline, guibg = cursorline })
-
-  hi('CmpItemAbbrMatch', { guifg = blue, gui = 'bold' })
-
-  hi('yamlTSField', 'TSKeyword')
-end
-
 local colorschemes = {
   -- 'base16-decaf',
   -- 'base16-eighties',
@@ -73,18 +41,12 @@ end, {
     desc = 'Select the next colorscheme',
   })
 
-local augroup = vim.api.nvim_create_augroup('cdejoye_base16_colorscheme', {})
-vim.api.nvim_create_autocmd('Colorscheme', {
-  callback = customize_theme,
-  pattern = 'base16-*',
-  group = augroup,
-})
+require('cdejoye.config.base16').setup()
 
 local vim_dir = vim.fn.fnamemodify(vim.env.MYVIMRC, ':h')
 local colorschemeFile = vim_dir .. '/colorscheme.vim'
-
 if vim.fn.filereadable(colorschemeFile) then
-  vim.cmd('source '.. colorschemeFile)
+  vim.cmd('source ' .. colorschemeFile)
 else
   vim.cmd('colorscheme base16-tomorrow-night')
 end
