0aee41a627b3ace567f12ba7811f5f9805410ead
Camille Dejoye <camilledejoye@gmail.com>
2025-08-09 11:58:15 +0200

    nvim(phpactor): fix class expand mapping to work in YAML files
    I finally removed my mapping repository.
    I created this a long time ago, thinking it would be nice to have a
    common set of mappings for all phpactor users and providing `<Plug>`
    mappings to help.
    The maintainer didn't agree with the idea of delivering default
    mappings, even if you could disable them. That add extra maintenance so
    I understood.
    Hence, the separate repo.
    
    But, I don't need an extra dependency, I can simply define my own
    mappings.

--- config/nvim/lua/cdejoye/plugins/php/phpactor.lua
+++ config/nvim/lua/cdejoye/plugins/php/phpactor.lua
@@ -3,37 +3,82 @@
 return {
   'phpactor/phpactor',
   build = 'composer install -o',
+  init = function()
+    vim.g.phpactorInputListStrategy = 'phpactor#input#list#fzf'
+    vim.g.phpactorQuickfixStrategy = 'phpactor#quickfix#fzf'
+  end,
   dependencies = {
-    {
-      'camilledejoye/phpactor-mappings',
-      init = function()
-        -- Enable while experimental
-        vim.g.phpactorInputListStrategy = 'phpactor#input#list#fzf'
-        vim.g.phpactorQuickfixStrategy = 'phpactor#quickfix#fzf'
-
-        -- phpactor-mapping
-        vim.g.phpactorActivateOverlapingMappings = true
-      end,
-    },
     'junegunn/fzf.vim',
     { 'junegunn/fzf', build = ':call fzf#install()' },
   },
-  ft = 'php',
   keys = {
     {
       '<Leader>pp',
-      '<Plug>phpactorContextMenu',
-      desc = '[Phpactor] Open context menu',
+      '<cmd>call phpactor#ContextMenu()<CR>',
+      desc = '[phpactor] Open context menu',
+      ft = 'php',
     },
     {
       '<Leader>pt',
-      '<Plug>phpactorTransform',
-      desc = '[Phpactor] Open transformation menu',
+      '<cmd>:call phpactor#Transform()<CR>',
+      desc = '[phpactor] Open transformation menu',
+      ft = 'php',
     },
     {
       '<Leader>pe',
-      '<Plug>phpactorClassExpand',
-      desc = '[Phpactor] Expand the class name, under the curor, as a FQCN',
+      '<cmd>call phpactor#ClassExpand()<CR>',
+      desc = '[phpactor] Expand the class name, under the curor, as a FQCN',
+      ft = { 'php', 'yaml' },
+    },
+    {
+      'cv',
+      '<cmd>set opfunc=phpactor#ExtractExpression<CR>g@',
+      desc = '[phpactor] Extract into variable',
+      ft = 'php',
+    },
+    {
+      'cv',
+      ':<C-U>call phpactor#ExtractExpression(visualmode())<CR>',
+      desc = '[phpactor] Extract into variable',
+      mode = 'x',
+      ft = 'php',
+    },
+    {
+      'cvv',
+      '<cmd>set opfunc=phpactor#ExtractExpression<CR>^g@$',
+      desc = '[phpactor] Extract into variable',
+      ft = 'php',
+    },
+    {
+      'cm',
+      '<cmd>set opfunc=phpactor#ExtractMethod<CR>g@',
+      desc = '[phpactor] Extract into method',
+      ft = 'php',
+    },
+    {
+      'cm',
+      ':<C-U>call phpactor#ExtractMethod(visualmode())<CR>',
+      desc = '[phpactor] Extract into method',
+      mode = 'x',
+      ft = 'php',
+    },
+    {
+      'cmm',
+      '<cmd>set opfunc=phpactor#ExtractMethod<CR>^g@$',
+      desc = '[phpactor] Extract into method',
+      ft = 'php',
+    },
+    {
+      'cc',
+      '<cmd>call phpactor#ExtractConstant()<CR>',
+      desc = '[phpactor] Extract into constant',
+      ft = 'php',
+    },
+    {
+      'yu',
+      '<cmd>call phpactor#UseAdd()<CR>',
+      desc = '[phpactor] Add import statement for the class name under the cursor',
+      ft = 'php',
     },
   },
 }
